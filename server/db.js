8d94639c70d006c055ecf9190d2eb5e6e4b4ae39
ArtenCool <artem04102021@gmail.com>
2022-12-30 19:58:05 +0300

    add server

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-module.experts = new Sequelize(
+module.exports = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
@@ -10,6 +10,3 @@ module.experts = new Sequelize(
         port: process.env.DB_PORT
     }
 );
-
-
-
--- server/index.js
+++ server/index.js
@@ -1,10 +1,27 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
+const sequelize = require('./db');
+const models = require('./models/modes');
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log(`Started server ${PORT}`)
+app.get('/', (req, res) => {
+    res.status(200).json({messaga: 'Working'})
 });
+
+const start = async () => {
+    try {
+        await sequelize.authenticate()
+        await sequelize.sync()
+        app.listen(PORT, () => console.log(`Started server ${PORT}`));
+    } catch(e) {
+        console.log(e);
+    }
+};
+
+start();
