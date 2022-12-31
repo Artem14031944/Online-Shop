4cc24f18ed2eb525ea08d1f5a4b1fdccfe2ac282
ArtenCool <artem04102021@gmail.com>
2022-12-31 20:32:18 +0300

    add uuid

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const fileUpload = require('express-fileupload');
 const sequelize = require('./db');
 const models = require('./models/modes');
 const router = require('./routes/index');
@@ -11,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload({}));
 app.use('/api', router);
+
 app.use(errorHandler);
 
 
